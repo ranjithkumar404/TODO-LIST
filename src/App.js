49ed6e247bb59b8e95da98eebf3abed901ff2476
ranjithkumar404 <ranjithkumarshetty808@gmail.com>
2023-06-01 18:27:23 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [val, setVal] = useState('')
   const [list, setList] = useState([])
 
   const fun = (e) => {
-    if (e.key === 'Enter') {
-      setList(val)
-    }
-    setVal('')
-    console.log(list);
+    e.preventDefault();
+  
+   
+      setList( [...list, val]);
+
+    
+    setVal("");
   }
+
   return (
     <div className="App flex justify-center  h-screen  items-center ">
       <div className="shadow-md p-3 rounded-md gird  place-items-center  w-[700px] ">
         <h1 className="text-2xl text-center">TODO-LIST</h1>
         <div className="">
-          <form className="flex p-1 space-x-3 justify-center" action="">
-            <input value={val} onChange={(e) => { setVal(e.target.value) }} onKeyUp={fun} className="outline-none rounded  p-1 bg-slate-200/50" type="text" />
-            <button onClick={() => { setList(val) }} className="bg-green-500 px-3 py-1 rounded-md text-white">Submit</button>
+          <form onSubmit={(e) => fun(e)} className="flex p-1 space-x-3 justify-center" action="">
+            <input value={val} onChange={(e) => { setVal(e.target.value) }}
+              className="outline-none rounded  p-1 bg-slate-200/50" type="text" />
+            <button className="bg-green-500 px-3 py-1 rounded-md text-white">Submit</button>
           </form>
         </div>
         <div>
           {
-            list.map((i) => {
+            list.map((i, a) => {
               return (
-                <p>{i}</p>
+                <div key={a}>
+                  <p>{i}</p>
+                </div>
               )
             })
           }
